7896b05ea29230b6745e48e35e37140df1e469ec
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2022-05-13 08:27:05 +0200

    Add README.md, tests and function signature

--- src/turingtoy/__init__.py
+++ src/turingtoy/__init__.py
@@ -1,3 +1,18 @@
+from typing import (
+    Dict,
+    List,
+    Optional,
+    Tuple,
+)
+
 import poetry_version
 
 __version__ = poetry_version.extract(source_file=__file__)
+
+
+def run_turing_machine(
+    machine: Dict,
+    input_: str,
+    steps: Optional[int] = None,
+) -> Tuple[str, List, bool]:
+    pass  # Implement the function
